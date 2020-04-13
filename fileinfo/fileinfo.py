33b9b9f7c939391ca1e79ddfcba02fdcfac2b5c4
Leonardo Giordani <giordani.leonardo@gmail.com>
2020-04-13 11:47:09 +0100

    Patch with context manager

--- fileinfo/fileinfo.py
+++ fileinfo/fileinfo.py
@@ -5,3 +5,10 @@ class FileInfo:
     def __init__(self, path):
         self.original_path = path
         self.filename = os.path.basename(path)
+
+    def get_info(self):
+        return (
+            self.filename,
+            self.original_path,
+            os.path.abspath(self.original_path)
+        )
--- tests/test_fileinfo.py
+++ tests/test_fileinfo.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 from fileinfo.fileinfo import FileInfo
 
 
@@ -12,3 +14,14 @@ def test_init_relative():
     relative_path = '../{}'.format(filename)
     fi = FileInfo(relative_path)
     assert fi.filename == filename
+
+
+def test_get_info():
+    filename = 'somefile.ext'
+    original_path = '../{}'.format(filename)
+
+    with patch('os.path.abspath') as abspath_mock:
+        test_abspath = 'some/abs/path'
+        abspath_mock.return_value = test_abspath
+        fi = FileInfo(original_path)
+        assert fi.get_info() == (filename, original_path, test_abspath)
